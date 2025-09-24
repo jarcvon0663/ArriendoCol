14210d0cd5fefc6718abf677fc055f9e2acc83ff
Jeison Arturo Rios Castaño <jarcvon@hotmail.com>
2025-09-23 20:03:25 -0500

    title

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ import '@/assets/styles/globals.css';
 import 'photoswipe/dist/photoswipe.css';
 
 export const metadata = {
-  title: 'PropertyPulse',
+  title: 'ArriendoCol',
   description: 'Find The Perfect Rental Property',
   keywords: 'rental, property, real estate',
 };
